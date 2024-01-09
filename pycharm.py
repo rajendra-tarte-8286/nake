aff3a2f5a996fab88dfbb216ba146406baa65432
rajendra tarte <rajtarte84@gmail.com>
2024-01-09 10:07:30 +0530

    ohh i add it on this way

---
+++ pycharm.py
@@ -0,0 +1,1 @@
+ sory i need to type here but i did on in different way what he thinks i am very good with that because i want her no way man what so ever he thinks is completely different i never want her in my life i am very different kind of person he thinks i am talking with very good manner may be he thinks beacause of her i am takling good with that man its completely wrong 
